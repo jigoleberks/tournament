d709f1336fc13542ef8ca83cfab260df74c74b8d
Galen Patterson <pattersongalen@gmail.com>
2026-05-10 11:07:19 -0600

    Drop unused body text column from club_rules_revisions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_10_160640) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_10_170553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -114,7 +114,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_10_160640) do
   end
 
   create_table "club_rules_revisions", force: :cascade do |t|
-    t.text "body"
     t.bigint "club_id", null: false
     t.datetime "created_at", null: false
     t.bigint "edited_by_user_id", null: false
