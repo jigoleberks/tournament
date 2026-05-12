264baf9f1a7368153c78bc51649e1a124bb62fd1
Galen Funk <aronfunk@sunsetcomputers.ca>
2026-05-11 21:06:21 -0600

    Add site-wide stats tiles to admin dashboard for site admins

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardsController < Admin::BaseController
   def index
+    return unless current_user&.admin?
+
+    @total_clubs          = Club.count
+    @total_active_members = User.active.count
+    @active_tournaments   = Tournament.where("ends_at IS NULL OR ends_at >= ?", Time.current).count
+    @catches_last_7_days  = Catch.where(captured_at_device: 7.days.ago..).count
   end
 end
